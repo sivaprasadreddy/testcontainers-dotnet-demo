cd78b124fab8d1c7c55935b3a459479992dea06d
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2023-09-15 20:45:48 +0530

    Fix TC config issue

--- BookmarksApi.Tests/IntegrationTestFactory.cs
+++ BookmarksApi.Tests/IntegrationTestFactory.cs
@@ -8,28 +8,24 @@ using Testcontainers.PostgreSql;
 public class IntegrationTestFactory<TProgram, TDbContext> : WebApplicationFactory<TProgram>, IAsyncLifetime
     where TProgram : class where TDbContext : DbContext
 {
-    private readonly PostgreSqlContainer _postgres;
-
-    public IntegrationTestFactory()
-    {
-        _postgres = new PostgreSqlBuilder()
-        .WithImage("postgres:15-alpine")
-        .WithResourceMapping("schema.sql", "/docker-entrypoint-initdb.d/")
-        .Build();
-    }
-
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
-        var connString = _postgres.GetConnectionString();
-        Console.WriteLine("TC ConnectionString="+connString);
-        
-        builder.ConfigureTestServices(services =>
-        {
-           services.AddDbContext<TDbContext>(options => { options.UseNpgsql(connString); });
-        });
-    }
+    private readonly PostgreSqlContainer _postgres =
+            new PostgreSqlBuilder()
+                     .WithImage("postgres:15-alpine")
+                     .WithResourceMapping("schema.sql", "/docker-entrypoint-initdb.d/")
+                     .Build();
 
     public async Task InitializeAsync() => await _postgres.StartAsync();
 
     public new async Task DisposeAsync() => await _postgres.DisposeAsync();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        var connString = _postgres.GetConnectionString();
+        //Console.WriteLine("TC ConnectionString="+connString);
+        builder.ConfigureTestServices(services =>
+        {
+            services.Remove(services.SingleOrDefault(service => typeof(DbContextOptions<TDbContext>) == service.ServiceType));
+            services.AddDbContext<TDbContext>(options => { options.UseNpgsql(connString); });
+        });
+    }
 }
